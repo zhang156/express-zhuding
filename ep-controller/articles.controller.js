2607a27b4671d281f16e651483c4c55d531e5c43
zhang156 <1028524971@qq.com>
2017-11-14 23:27:56 +0800

    配置服务端origin和header允许跨域

--- ep-controller/articles.controller.js
+++ ep-controller/articles.controller.js
@@ -2,7 +2,7 @@ var {handleRequest} = require('../ep-utils/ep-handle')
 var articleCtrl = {list: {}, item: {}}
 
 articleCtrl.list.GET = (req, res) => {
-    res.json({article: [1,2,3]})
+    res.json({article: [1,2,3,4,5]})
 }
 
 exports.list = (req, res) => {
--- ep-routes.js
+++ ep-routes.js
@@ -5,6 +5,18 @@ module.exports = app => {
 
     // 拦截器
     app.all('*', (req, res, next) => {
+        // 配置允许跨域
+        const allowedOrigins = ['https://surmon.me', 'https://admin.surmon.me'];
+		const origin = req.headers.origin || '';
+		if (allowedOrigins.includes(origin) || origin.includes('localhost')) {
+			res.setHeader('Access-Control-Allow-Origin', origin);
+		};
+		res.header('Access-Control-Allow-Headers', 'Authorization, Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With');
+		res.header('Access-Control-Allow-Methods', 'PUT,PATCH,POST,GET,DELETE,OPTIONS');
+		res.header('Access-Control-Max-Age', '1728000');
+		res.header('Content-Type', 'application/json;charset=utf-8');
+		res.header('X-Powered-By', 'Nodepress 1.0.0');
+
         next();
     })
 

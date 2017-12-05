1ff6761201f3c6394a50e56b77307c0e2c570681
zhang156 <1028524971@qq.com>
2017-12-05 16:15:50 +0800

    update

--- ep-config.js
+++ ep-config.js
@@ -5,7 +5,7 @@ exports.APP = {
 }
 
 exports.MONGODB = {
-	uri: `mongodb://127.0.0.1:${argv.dbport || '27017'}/express-zhuding`,
+	uri: `mongodb://${argv.db_username}:${argv.db_password }@66.55.159.180:${argv.dbport || '27017'}/express-zhuding`,
 	username: argv.db_username || 'DB_username',
 	password: argv.db_password || 'DB_password'
 }
